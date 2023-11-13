5885948553f4fd8454a796b5a40ad0e921fbf311
yogendrachereddy <122440913+yogendrachereddy@users.noreply.github.com>
2023-11-13 19:56:08 +0530

    Add files via upload

---
+++ FCFS.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+   int N;
+   printf("Enter the no of process:");
+   scanf("%d",&N);
+   int Process[N],Burst[N];
+   int Gantch[N],i;
+   Gantch[0]=0;
+   float a_wt=0,a_tt=0;
+   printf("Enter the Burst Time:\n");
+   for(i=0;i<N;i++)
+   {
+       Process[i]=i+1;
+       printf("Enter the burst time for P%d Process:",Process[i]);
+       scanf("%d",&Burst[i]);
+   }
+   for(i=0;i<N;i++)
+   { 
+     if(i==0){
+        Gantch[i]=Gantch[i]+Burst[i];
+        a_wt=0;
+        a_tt=Gantch[i];
+     }
+    else {
+        Gantch[i]=Gantch[i-1]+Burst[i];
+        a_wt=a_wt+Gantch[i-1];
+        a_tt=a_tt+Gantch[i];
+    }
+   }
+   a_wt=(float)a_wt/N;
+ a_tt=(float)a_tt/N;
+   printf("Average Waiting time:%.2f",a_wt);
+   printf("Average Turnaroung time %.2f",a_tt);
+  return 0;;
+}
